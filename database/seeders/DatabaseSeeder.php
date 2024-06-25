85440bed7ed69748fc212dbd6a8673a99c918e67
vdoann <openaivdoan@gmail.com>
2024-06-25 10:57:52 +0700

    Project hoc tap tren lop

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin'
+        ]);
+
+        \App\Models\User::factory(10)->create();
+
+    }
+}
